c87f249d52606b6be002651faae44e20217412aa
G.Vignesh Krishna <112069603+GVK2100030980@users.noreply.github.com>
2024-01-04 16:29:18 +0530

    Create Program.java

---
+++ Program.java
@@ -0,0 +1,9 @@
+public class Main
+{
+        public static void main(String args[])
+        {
+                int n1 = 223, n2 = 130,sum;
+                sum = n1+n2;
+                System.out.println("The sum is :" + sum);
+        }
+}
